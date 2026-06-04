f7f666da45191f0e20a5b671e28e7b5654d86f54
João Lucas Rodrigues <54967700+joaosp7@users.noreply.github.com>
2026-06-04 19:41:03 -0300

    hello world from bot

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
+import os
+from dotenv import load_dotenv
+import asyncio
+import telegram
+
+load_dotenv()
+
+async def main():
+    bot_token = os.getenv("BOT_TOKEN")
+    if (not bot_token):
+        raise RuntimeError("Missing Bot Token.")
+    bot = telegram.Bot(bot_token)
+    async with bot:
+        print(await bot.get_me())
     print("Hello from py-bot!")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
